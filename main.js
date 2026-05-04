3f9a0d740e9f8036a49c2596d2d1d6a8829ffb80
GuilhermeSarter <guilherme.sarter@escola.pr.gov.br>
2026-05-04 10:11:15 -0300

    sad

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
+const abas = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0; i <botoes.length; i++){
-    botoes[i].onlick = function (){
-        for(let j=this.onfullscreenchange; j <botoes.length; j++)}}
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function() {
+        // Remove classe ativo de todos os botões e abas
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            abas[j].classList.remove("ativo");
+        }
+        // Adiciona classe ativo ao botão e aba clicados
+        this.classList.add("ativo");
+        abas[i].classList.add("ativo");
+    }
+}
